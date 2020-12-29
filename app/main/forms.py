1f7e6d6bb2d53a854ef68f7ce3a252fb9e375933
Globe-Eater <bullokl@ostatemail.okstate.edu>
2020-12-29 16:02:17 -0600

    Custom forms started.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,1 +1,12 @@
-import wtforms
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp
+from wtforms import ValidationError
+from ..models import Role, User
+
+class QueryForm(FlaskForm):
+    property = StringField('Property name:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class SearchForm(FlaskForm):
+    submit = SubmitField('Sumbit')
