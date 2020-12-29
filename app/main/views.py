4fe4de187a7c02836f9795bc8d69f4597280212d
Globe-Eater <bullokl@ostatemail.okstate.edu>
2020-12-29 16:02:49 -0600

    routes started.

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, abort, flash, request,\
-    current_app, make_reponse
+    current_app, make_response
 from flask_login import login_required, current_user
-from flask_sqlalchmey import get_debug_queries
+from flask_sqlalchemy import get_debug_queries
 from . import main
 from .forms import SearchForm, QueryForm
 from .. import db
@@ -10,6 +10,8 @@ from ..models import Permission, Role, User
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = SearchForm()
+    if form.validate_on_submit():
+        return redirect('query.html')
     return render_template('index.html', form=form)
 
 @main.route('/search', methods=['GET', 'POST'])
