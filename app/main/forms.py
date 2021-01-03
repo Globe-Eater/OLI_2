5fdf0f41a09ca0efe56bc81a0cba4d01c553a7fc
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-01-02 19:02:35 -0600

    Addtional fields are now queriable.

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,6 +6,9 @@ from ..models import Role, User
 
 class QueryForm(FlaskForm):
     property = StringField('Property name:', validators=[DataRequired()])
+    resname = StringField('Resource name:', validators=[DataRequired()])
+    address = StringField('Address: ', validators=[DataRequired()])
+    city = StringField('City name: ', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class SearchForm(FlaskForm):
