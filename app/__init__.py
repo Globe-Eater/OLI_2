4ca6244f2fa4505b78e1968531fa65453a9663b9
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-01-13 16:03:09 -0600

    Login manager added.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
@@ -9,10 +10,15 @@ moment = Moment()
 db = SQLAlchemy()
 moment = Moment()
 
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    login_manager.init_app(app)
 
     bootstrap.init_app(app)
     db.init_app(app)
