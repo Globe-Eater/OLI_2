557e0da6173b64ec612f73a479b7b753c9510d4a
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-01-13 22:51:53 -0600

    Expanded user premissions tests.

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,7 +1,20 @@
 import unittest
-from app.models import User
+from app import create_app, db
+from app.models import User, AnonymousUser, Role, Permission
 
 class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
     def test_password_setter(self):
         u = User(password = 'cat')
         self.assertTrue(u.password_hash is not None)
@@ -10,7 +23,7 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password = 'cat')
         with self.assertRaises(AttributeError):
             u.password
-
+    
     def test_password_verification(self):
         u = User(password = 'cat')
         self.assertTrue(u.verify_password('cat'))
@@ -20,3 +33,38 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         u2 = User(password-'cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.SEARCH))
+        self.assertTrue(u.can(Permission.ENTRY))
+        self.assertTrue(u.can(Permission.EDIT))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_moderator_role(self):
+        r = Role.query.filter_by(name='Moderator').first()
+        u = User(email='john@example.com', password='cat', role=r)
+        self.assertTrue(u.can(Permission.SEARCH))
+        self.assertTrue(u.can(Permission.ENTRY))
+        self.assertTrue(u.can(Permission.EDIT))
+        self.assertTrue(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_administrator_role(self):
+        r = Role.query.filter_by(name='Administrator').first()
+        u = User(email='john@example.com', password='cat', role=r)
+        self.assertTrue(u.can(Permission.SEARCH))
+        self.assertTrue(u.can(Permission.ENTRY))
+        self.assertTrue(u.can(Permission.EDIT))
+        self.assertTrue(u.can(Permission.MODERATE))
+        self.assertTrue(u.can(Permission.ADMIN))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.SEARCH))
+        self.assertFalse(u.can(Permission.ENTRY))
+        self.assertFalse(u.can(Permission.EDIT))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
