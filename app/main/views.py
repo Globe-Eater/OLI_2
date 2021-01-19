a09fe12ac03daaef065e63bfc44ca5a5863056df
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-01-19 14:47:49 -0600

    results page added.

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,10 @@
 from flask import render_template, redirect, url_for, abort, flash, request,\
     current_app, make_response
-from flask_login import login_required, current_user
 from flask_sqlalchemy import get_debug_queries
 from . import main
 from .forms import SearchForm, QueryForm
 from .. import db
-from ..models import Permission, Role, User
+from ..models import Permission, Role, User, hpr
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,4 +16,10 @@ def index():
 @main.route('/query', methods=['GET', 'POST'])
 def search():
     form = QueryForm()
+    if form.validate_on_submit():
+        return render_template('results.html')
     return render_template('query.html', form=form)
+
+@main.route('/results')
+def search_results():
+    return render_template('results.html')
