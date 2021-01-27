140f18951e602eb2593b49ffdcc242c940314ad7
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-01-27 14:05:20 -0600

    auth.menu calls the successful login menu page.

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,7 +13,7 @@ def user_login():
         user = User.query.filter_by(email=form.email.data.lower()).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return render_template('auth/success.html')
+            return redirect(url_for('auth.menu'))
         flash('Invalid email or password.')
     return render_template('auth/login.html', form=form)
 
@@ -24,7 +24,7 @@ def logout_user():
     flash('You have been successfully logged out.')
     return redirect(url_for('main.index'))
 
-@auth.route('/success')
+@auth.route('/success', methods=['GET', 'POST'])
 @login_required
 def menu():
     form = MenuForm()
