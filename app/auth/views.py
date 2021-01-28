9787f231cf1fc52bc9f294f73b9a9ad6a7a448c8
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-01-28 10:28:00 -0600

    Record Entry Form started. Has not been tested to upload into database.

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask_login import login_user, logout_user, login_required, \
 from . import auth
 from .. import db
 from ..models import User
-from .forms import LoginForm, MenuForm
+from .forms import LoginForm, MenuForm, EntryForm 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def user_login():
@@ -20,7 +20,6 @@ def user_login():
 @auth.route('/logout')
 @login_required
 def logout_user():
-    logout_user()
     flash('You have been successfully logged out.')
     return redirect(url_for('main.index'))
 
@@ -28,7 +27,20 @@ def logout_user():
 @login_required
 def menu():
     form = MenuForm()
+    if form.validate_on_submit():
+        return redirect(url_for('auth.data_entry'))
     return render_template('auth/success.html', form=form)
+
+@auth.route('/record_entry', methods=['GET', 'POST'])
+@login_required
+def data_entry():
+    form = EntryForm()
+    if form.validate_on_submit():
+        record = hpr(propname=form.propname.data)
+        db.session.add(record)
+        db.session.commit()
+    flash('Record Sumbitted.')
+    return render_template('auth/entry.html', form=form)
 
 @auth.route('/unconfirmed')
 def unconfirmed():
