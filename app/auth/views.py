3bff1172fe1ca794b50962fa4e7e1e8bccc67fe4
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-01-29 17:52:24 -0600

    Login/Logout functionally working as intented. Protects webpages that require login now.

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,7 +19,8 @@ def user_login():
 
 @auth.route('/logout')
 @login_required
-def logout_user():
+def acc_logout():
+    logout_user()
     flash('You have been successfully logged out.')
     return redirect(url_for('main.index'))
 
