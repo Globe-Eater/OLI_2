de5164e18cf8094f7a2184bc53095527ec4b71e2
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-02-12 18:14:59 -0600

    Added coverage to shell env. Also added seleinum package to env.

--- OLI_2.py
+++ OLI_2.py
@@ -1,12 +1,15 @@
 import os
-# dotenv_path
-# Coverage
-
 import sys
 import click
 # migrate
 from app import create_app, db
 from app.models import User, Role, Permission
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 # migrate = Migrate(app, db)
@@ -24,8 +27,17 @@ def test(coverage, test_names):
     if coverage and not os.environ.get('FLASK_COVERAGE'):
         import subprocess
         os.environ['FLASK_COVERAGE'] = '1'
-        sys.exit(subprocess.call(sys.argv))
-
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
